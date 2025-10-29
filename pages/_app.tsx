ba0b15cb27ddd145eb09caf53ddf146f620987c3
patvath <prophecypat@gmail.com>
2025-10-29 13:25:05 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import "../styles/theme.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="flex">
+    <div className="flex bg-[#0A0F24] text-white min-h-screen">
       <Sidebar />
-      <main className="flex-1 ml-60 bg-[#0A0F24] min-h-screen text-white p-6">
+      <main className="flex-1 ml-60 p-6">
         <Component {...pageProps} />
       </main>
     </div>
